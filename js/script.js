3f70fc40746000e8ee5e988a065b5eba809046ad
Igor <igorrr1138@gmail.com>
2020-10-23 18:04:30 +0300

    firsst commit, added new file main.css

---
+++ js/script.js
@@ -0,0 +1,41 @@
+" use strict";
+
+// alert ("Hello");
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = +prompt("Are you 18", "18");
+// console.log(answer + 200);
+
+// const answers = [];
+
+// answers [0] = prompt('Как ваше имя?', '');
+// answers [1] = prompt('Как ваша фамилия?', '');
+// answers [2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/8`);
+
+// const user = 'Vanyara';
+// alert(`Здароваааа!, ${user}, Как дела, а????`);
+
+// console.log(12 + +'3');
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(2*4 === +'8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
